5ac540145333179867e9a65c3ff193092ae62ba8
SalimNoori <salimshah.noori@seattlecolleges.edu>
2024-05-14 23:05:54 -0700

    type

--- jsonSpr24_1010class.py
+++ jsonSpr24_1010class.py
@@ -1,19 +1,17 @@
-#Importing the 'json' module to work with JSON data.
+# Importing the 'json' module to work with JSON data.
 import json
 
-#Creating a dictionary named 'data' containing information about myself.
+# Creating a dictionary named 'data' containing information about myself.
 data = { 
     'name': 'John Doe',
-    'age':29, 
+    'age': 29, 
     'city': 'Washington, WA', 
-    'interests': ['Traveling','Cricket','Soccer','Hiking','Studying'],
-    'is_studnet': True 
-
+    'interests': ['Traveling', 'Cricket', 'Soccer', 'Hiking', 'Studying'],
+    'is_student': True 
 }
 
+# Creating a JSON and writing the Python object contents to the JSON file
+with open('data.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
 
-#
-with open('data.json','w') as json_file:
-    json.dump(data,json_file,indent=4)
-
-print("Data has been written to datal.Json")
+print("Data has been written to data.json")
